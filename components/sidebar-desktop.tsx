f9feb691b4a02bed9d147464471464ab411bc0f8
Parsa Asgari <parsa.asgari3264@gmail.com>
2024-05-08 14:12:25 +0100

    added a few translation fixes

--- components/sidebar-desktop.tsx
+++ components/sidebar-desktop.tsx
@@ -5,6 +5,8 @@ import { ChatHistory } from '@/components/chat-history'
 import { SideBarAdmin } from '@/components/sidebar-administration'
 import { SideBarBookmarks } from '@/components/sidebar-bookmarks'
 import { Separator } from "@/components/ui/separator"
+import { GetTranslation } from "@/components/translation-helper/ClientTranslations"
+
 export async function SidebarDesktop() {
   const session = await authUser()
 
@@ -16,7 +18,7 @@ export async function SidebarDesktop() {
     <Sidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
       {/* @ts-ignore */}
       <h4 className="mb-2 px-2 pt-2 text-xl font-bold">
-                Navigation
+                <GetTranslation text="Navigation" />
             </h4>
       <SideBarBookmarks />
       <SideBarAdmin />
