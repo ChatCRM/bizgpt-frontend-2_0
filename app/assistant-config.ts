406911d27b701d4ec3fe2c4155b87033a58496d2
Ali <amohammadzadeh@interrodata.com>
2024-08-02 17:55:03 +0330

    fix compile issue

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,5 @@
 export let assistantId = 'asst_wMFxvPdrI4G3l09TxYkAB2HH' // set your assistant ID here
 
 if (assistantId === '') {
-  assistantId = process.env.OPENAI_ASSISTANT_ID
+  assistantId = process.env.OPENAI_ASSISTANT_ID!
 }
