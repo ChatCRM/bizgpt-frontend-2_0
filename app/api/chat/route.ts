a657db86ad06597733e67687695829ca9e4199ca
Ali <amohammadzadeh@interrodata.com>
2024-10-15 14:21:17 +0330

    update prompt with knowledge of CSV files structure

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -63,6 +63,226 @@ export async function POST(req: Request) {
     Profit and Loss Statement: file-Tt6L4Xo91vK5Ms7tTszpeyKV
     Financial Voucher: file-QZOTEtkQegWhYMEHkbogTBLP
 
+      The CSV FIles have the following structures:
+
+      ProfitAndLoss CSV FIle:
+      ID (bigint)
+      YearID (nvarchar)
+      StatementCode (int)
+      StatementTitle (varchar)
+      Amount (int)
+
+      FinancialVoucher CSV FIle:
+      ID (bigint)
+      AccountNumber (int)
+      AccountName (varchar)
+      AccountGroupName (varchar)
+      VoucherDate (date)
+      Amount (int)
+
+      FinancialVoucher CSV File values are like the following:
+
+      AccountName has the following unique values:
+          Meeting course update and the like (ki;
+          Reporting group life;
+          Postage;
+          Rental of computer systems licenses and equipment;
+          Outbound value added tax when purchasing services from abroad low rate;
+          Travel cost excluding VAT deduction;
+          Accounting reconciliation reporting payroll;
+          Accrued interest income;
+          Gifts tax deductible;
+          IT Group;
+          Contingent deductible;
+          Costs Debt collection services;
+          Group sales income taxable;
+          Counter account reimbursement sick pay;
+          Preliminary project implementation;
+          Consulting;
+          Fixtures and equipment;
+          Other office expenses;
+          Income from previously written-down receivables;
+          Reported otp;
+          Intangible assets;
+          Earned unbilled income;
+          Xledger;
+          Small procurement inventory;
+          Market Group;
+          Accrued costs of goods sold;
+          Other requirements company same group;
+          Xledger licenses;
+          Holiday pay calculated;
+          Depreciation of intangible assets and GW;
+          Loss on receivables;
+          Small procurement computer equipment;
+          Accrual account salary Gr. 50xx;
+          Sales revenue group tax-free;
+          Interim bank account;
+          Provision for losses on receivables;
+          Deductible benefits in kind;
+          Counter account reporting group life;
+          Rent premises;
+          Fees for legal assistance - deductible;
+          Operating account 7058.06.69520;
+          Gain on realization of shares;
+          Employer's tax;
+          Observation account;
+          Currency loss (disagio);
+          Foreign exchange gain (agio);
+          Prepaid costs;
+          Payable employer's tax;
+          Annual statement and equation;
+          Opposite account Reported otp;
+          Bank withholding tax;
+          Auxiliary account onward invoicing Group;
+          Accounts receivable group companies;
+          Tripletex;
+          Accounts payable;
+          Cleaning;
+          Other cost;
+          Other short-term receivables;
+          Income Fee/interest Debt collection services;
+          Accounts payable tax return VAT;
+          Consultant's fee Group;
+          Visma licenses;
+          Cost of goods;
+          Output VAT low rate;
+          Portfolio Transactions;
+          Unsettled customer records;
+          Investments in subsidiaries;
+          Client funds;
+          Mobile phones / equipment;
+          Accrued aga holiday pay;
+          Warehouse rental/parking;
+          Interest income bank;
+          Accounting fee Group;
+          Receivable dividend;
+          To further fact.;
+          Other staff costs;
+          Salary to employees;
+          Decr. machines fixtures etc.;
+          System cost Group;
+          Other foreign service;
+          Accrual income;
+          Other cost of sales;
+          Light heat;
+          Mobile phone and feature employees;
+          Overtime;
+          Other cost premises;
+          PowerOffice Go;
+          Rounding account;
+          Accounting work/trans processing property;
+          Allocation of overtime and overtime;
+          Hourly wages;
+          Visma Kickback;
+          Reported sick pay reimbursement;
+          Salary/sick pay without holiday pay;
+          Gift to employees not deductible;
+          Client account;
+          Payroll Group;
+          Salary distribution between departments;
+          System revenue;
+          Training continuing education employees;
+          Flex;
+          Overtime food;
+          Accounting work free of charge;
+          OTP;
+          Advance deductions;
+          Offsetting account for other 52xx;
+          Bank and card fees;
+          Temp agency;
+          Dividend Mutual;
+          Receivable Group contribution;
+          Office supplies;
+          Re-invoiced avg obligatory;
+          Subletting;
+          Fee Group;
+          Advertising cost;
+          Holiday pay;
+          Property management;
+          Debt to company s. group;
+          Reimbursements of sick pay;
+          Other financial income;
+          Goods Group;
+          Narf Quotas;
+          Other operational rel. income deductable;
+          Input VAT when purchasing services from abroad low rate;
+          Visma.net;
+          Rental of office machines and equipment;
+          Fee audit;
+          Accounts payable group companies;
+          Common costs;
+          Arb. dept. accrued holiday pay;
+          Pull parking;
+          Input VAT medium rate raw fish etc.;
+          Accounts receivable;
+          Canteen cost;
+          Change in AVS losses on receivables;
+          Receivables from employees;
+          Input VAT low rate;
+          Accrued interest;
+          Travel costs not mandatory;
+          Car allowance compulsory;
+
+      AccountGroupName has the following unique values:
+          LICENSE PATENT LICENSES
+          OTHER FINANCIAL COSTS
+          OTHER OPERATING EXPENSES
+          PUBLIC FEES OBLIGED
+          SUPPLIER DEBTS
+          OTHER CLAIMS
+          CLAIMS ON EMPLOYEES
+          DEPRECIATION
+          OTHER CARD DEBT
+          OTHER SALES INCOME
+          BANK DEPOSITS AND CASH
+          SALES INCOME
+          OTHER FINANCIAL INCOME
+          LABOR COST
+          OTHER INTEREST INCOME
+          CUSTOMER RECEIVABLES
+          FIXED INVENTORY
+          COST OF GOODS
+          RECEIVABLES
+          INVESTMENT SUBSIDIARY
+
+      VoucherDate format is like '2022-04-30'
+
+      ProfitAndLoss Table values are like the following:
+
+      YearID  format is like '2022'
+
+      StatementCode  format is like 10
+
+      StatementTitle has the following unique values:
+          Cost of goods
+          Earnings
+          Extraordinary costs
+          Extraordinary revenues
+          Income from investments in subsidiaries
+          Income on investment. in an affiliate
+          Income on investment. Other enterprise in SME group
+          Inventories
+          Labor costs
+          Net income
+          Ordinary depreciation
+          Other finance costs
+          Other operating costs
+          Other operating income
+          Profit After tax
+          Profit before tax
+          Tax
+          Tax extraordinary
+          Total Cost
+          Total finance costs
+          Total financial income
+          Total operating revenues
+          Total other finance costs
+          Total other financial income
+          Total other interest expense
+          Total other interest income
+          Total sales revenue
+
     Your task is to analyze these files and answer any questions related to their content. You should:
 
         Provide the final answer without additional details unless explicitly asked.
